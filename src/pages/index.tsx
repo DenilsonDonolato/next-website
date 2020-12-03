befc600bd1bf7c515009dbcfcb449d8ef138cea0
Denilson Donolato <42578438+DenilsonDonolato@users.noreply.github.com>
2020-12-03 13:53:56 -0300

    Prettier config

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,11 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
 
 type Props = {
-  title: string,
-}
+  title: string;
+};
 
-export default function Home({ title = 'My Website' } : Props) {
+export default function Home({ title = 'My Website' }: Props) {
   return (
     <div className={styles.container}>
       <Head>
@@ -66,5 +66,5 @@ export default function Home({ title = 'My Website' } : Props) {
         </a>
       </footer>
     </div>
-  )
+  );
 }
