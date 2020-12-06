00f539096fce2a279d6690f747d61cf793b7152e
denilsondonolato <42578438+DenilsonDonolato@users.noreply.github.com>
2020-12-06 09:27:53 -0300

    Add jest support

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.export = {
+  testEnvironment: 'jsdon',
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.ts(x)'],
+  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts']
+};
